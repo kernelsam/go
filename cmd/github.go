5c0e1bb7341834825bb05af2dc16b64849bae0b1
kernelsam <kernelsam@users.noreply.github.com>
2023-08-02 23:42:11 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.0.3

---
+++ cmd/github.go
@@ -0,0 +1,13 @@
+// 0.0.3
+// Created by make-go-github-file.yaml on Wed Aug  2 23:42:11 UTC 2023
+package cmd
+
+var (
+	githubDate           string = "2023-08-02"
+	githubIteration      string = "0"
+	githubRef            string = "refs/tags/0.0.3"
+	githubRefName        string = "0.0.3"
+	githubRepository     string = "kernelsam/go"
+	githubRepositoryName string = "go"
+	githubVersion        string = "0.0.3"
+)
