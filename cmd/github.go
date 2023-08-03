6a4e85c374c7de51c75aa916226e254185fda722
109683132+kernelsam <109683132+kernelsam@users.noreply.github.com>
2023-08-03 00:33:24 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.0.7

---
+++ cmd/github.go
@@ -0,0 +1,13 @@
+// 0.0.7
+// Created by make-go-github-file.yaml on Thu Aug  3 00:33:24 UTC 2023
+package cmd
+
+var (
+	githubDate           string = "2023-08-03"
+	githubIteration      string = "0"
+	githubRef            string = "refs/tags/0.0.7"
+	githubRefName        string = "0.0.7"
+	githubRepository     string = "kernelsam/go"
+	githubRepositoryName string = "go"
+	githubVersion        string = "0.0.7"
+)
